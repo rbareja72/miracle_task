212954ca4cfd6d54631b088d5aadcf16449c8199
Rahul Bareja <rbareja72@gmail.com>
2020-07-19 01:19:54 +0530

    initialzing app with default values

--- App/index.js
+++ App/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import Router from './src/config/routes';
 import store from './src/config/store';
+import { getData, storeData } from './src/utils/dataUtil';
+import { DEFAULT_EMAIL, DEFAULT_PASSWORD } from './src/config/constants';
 
 const App = () => {
+
+  useEffect(() => {
+    setDefaultData();
+  }, []);
+
+  /**
+   * this function checks if email & password exists,
+   * if not then set default in the storage.
+   */
+  const setDefaultData =  async () => {
+    const storedEmail = await getData('email');
+    const storedPassword = await getData('password');
+    if (!(storedEmail && storedPassword)) {
+      storeData('email', DEFAULT_EMAIL);
+      storeData('password', DEFAULT_PASSWORD);
+    }
+  };
+
   return (
     <View style={styles.flexStyle}>
       <Provider store={store}>
---
+++ App/src/config/constants.js
@@ -0,0 +1,3 @@
+export const DEFAULT_EMAIL = 'abc@gmail.com';
+export const DEFAULT_PASSWORD = '12345678';
+export const DEFAULT_OTP = '12345';
